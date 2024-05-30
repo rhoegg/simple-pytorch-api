f147d343469e313d9ea1ed8f4171d03b64632f8f
Ryan Hoegg <ryan.hoegg@gmail.com>
2024-05-30 16:10:56 -0500

    basic model creation, loading, exporting, and prediction

--- app.py
+++ app.py
@@ -1,8 +1,62 @@
-from fastapi import FastAPI
+from model import *
+from fastapi import FastAPI, Request, Response, status
+from typing import List
+import io
+import uuid
 import torch
 
-app = FastAPI()
 
-@app.post("/models")
+device = (
+	"cuda"
+	if torch.cuda.is_available()
+	else "mps"
+	if torch.backends.mps.is_available()
+	else "cpu"
+)
+print(f"Using {device} device")
+
+app = FastAPI()
+models = {}
+
+@app.get("/")
 async def root():
-	return str(torch.rand(5, 3))
+	return {}
+
+@app.post("/models/", status_code=status.HTTP_201_CREATED)
+async def create_model(spec: NeuralNetSpec):
+	model_id = str(uuid.uuid4())
+	models[model_id] = NeuralNet(spec)
+	return { "id": model_id }
+
+@app.post("/models/{model_id}/predict", response_model=List[float])
+async def predict(model_id: str, inputs: List[float]):
+	if model_id not in models:
+		response.status_code = 404
+		return response
+	m = models.get(model_id)
+	nn_output = m.forward(torch.FloatTensor(inputs))
+	return nn_output.tolist()
+
+@app.get("/models/{model_id}", response_class=Response)
+async def get_model(model_id: str):
+	if model_id not in models:
+		return Response(status_code=404)
+	m = models.get(model_id)
+	return Response(status_code=200, content=m.export(), media_type="application/octet-stream")
+
+@app.put("/models/{model_id}", openapi_extra={
+	"requestBody": {
+		"content": {
+			"application/octet-stream": {
+				"schema": {
+					"type": "string",
+					"format": "binary"
+				}
+			}
+		}
+	}
+})
+async def replace_model(model_id: str, request: Request):
+	with io.BytesIO(await request.body()) as buf:
+		models[model_id] = NeuralNet.from_saved(buf)
+	#torch_model.forward(torch.rand((1, 48)))
--- model.py
+++ model.py
@@ -1,8 +1,9 @@
+import io
 from pydantic import BaseModel
+from torch import nn, load, save
 
-class NeuralNetSpec(BaseModel):
-    layers: list
-    modifiers: list
+input_features = 48
+output_features = 4
 
 class NeuralNetLayerSpec(BaseModel):
     outputs: int
@@ -11,3 +12,77 @@ class NeuralNetLayerSpec(BaseModel):
 class NeuralNetModifierSpec(BaseModel):
     name: str
     parameters: dict
+
+class NeuralNetSpec(BaseModel):
+    layers: list[NeuralNetLayerSpec]
+    modifiers: list[NeuralNetModifierSpec]
+
+class NeuralNet(nn.Module):
+    def __init__(self, nn_spec: NeuralNetSpec):
+        super(NeuralNet, self).__init__()
+        
+        self.spec = nn_spec
+        self.layer_stack = nn.Sequential()
+        layer_input_features = input_features
+        for layer in nn_spec.layers:
+            self.layer_stack.append(nn.Linear(layer_input_features, layer.outputs))
+            layer_input_features = layer.outputs
+            self.layer_stack.append(parse_activation(layer))
+        self.layer_stack.append(nn.Linear(layer_input_features, output_features))
+
+        # output_loss_delta = next(
+        #     (m.parameters.delta for m in nn_spec.modifiers if m.parameters.has_key("delta")),
+        #     1.0)
+        # print("huber loss delta = " + output_loss_delta)
+
+    @classmethod
+    def from_saved(cls, data: bytes):
+        return load(data)
+    
+    def export(self):
+        #onnx_program = torch.onnx.dynamo_export(m, torch.rand((1, input_features), dtype=torch.float32))
+        with io.BytesIO() as buf:
+            save(self, buf)
+            buf.seek(0)
+            return buf.read()
+
+    def forward(self, inputs):
+        return self.layer_stack(inputs)
+
+def parse_activation(layer_spec: NeuralNetLayerSpec):
+    if "activation" not in layer_spec.parameters:
+        return nn.Identity()
+    match layer_spec.parameters.get("activation"):
+        case "relu":
+            return nn.ReLU()
+        case "sigmoid":
+            return nn.Sigmoid()
+        case "softmax":
+            return nn.Softmax()
+        case "identity":
+            return nn.Identity()
+        case "relu6":
+            return nn.ReLU6()
+        case "celu":
+            return nn.CELU()
+        case "elu":
+            return nn.ELU() # alpha param?
+        case "tanh":
+            return nn.Tanh()
+        case "mish":
+            return nn.Mish()
+        case "softplus":
+            return nn.Softplus() # beta, threshold?
+        case "hardswish":
+            return nn.Hardswish()
+        case "gelu":
+            return nn.GELU()
+        case "rrelu":
+            return nn.RReLU()
+        case "hardtanh":
+            return nn.Hardtanh()
+        case "tanhshrink":
+            return nn.Tanhshrink()
+        case "hardsigmoid":
+            return nn.Hardsigmoid()
+    return nn.Identity()
